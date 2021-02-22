1a7cdf47f1a68d62594feb94f53c105064ce0c32
KRPINF <korapin.infueng@gmail.com>
2021-02-22 21:23:34 +0700

    up

---
+++ 22012.py
@@ -0,0 +1,58 @@
+import os
+import redis
+import json
+from flask import Flask,request,jsonify
+
+app = Flask(__name__)
+db= redis.StrictRedis(
+        host='node9158-advweb-20.app.ruk-com.cloud',
+        port=11150,
+        password='QLXmve62151',
+        decode_responses=True
+        )
+
+#Get All
+@app.route('/',methods=['GET'])
+def Show_key():
+    name=db.keys()
+    name.sort()
+    req = []
+    for r in name :
+        req.append(db.hgetall(r))
+    return jsonify(req)
+
+
+# Get Single
+@app.route('/<Key>', methods=['GET'])
+def Show_singel(Key):
+    showSingel = db.hgetall(Key)
+    return showSingel
+
+#Update 
+@app.route('/KPOP/<Key>', methods=['PUT'])
+def Show_Update(Key):
+    user = request.json['user']
+    type = request.json['type']
+    user = {"id":Key, "user":user, "type":type}
+    db.hmset(Key,user)
+    return "Create Success"
+
+#Create
+@app.route('/KPOP', methods=['POST'])
+def Show_Create():
+    id = request.json['id']
+    user = request.json['user']
+    type = request.json['type']
+    user = {"id":id, "user":user, "type":type}
+    db.hmset(id,user)
+    return "Create Success"
+    
+#Delete
+@app.route('/<Key>', methods=['DELETE'])
+def Show_Del(Key):
+    db.delete(Key)
+    return "Delete Success"
+
+if __name__ == '__main__':
+    app.run()
+#    app.run(host='0.0.0.0', port=81)
